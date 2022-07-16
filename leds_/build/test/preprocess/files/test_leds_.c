8437dcf12d4cf6d14e7014a87c2701f93070844a
ldiazcharris <lddiazcharris@gmail.com>
2022-07-16 09:47:04 -0500

    commit inicial - creacion del proyecto ceedling

---
+++ leds_/build/test/preprocess/files/test_leds_.c
@@ -0,0 +1,10 @@
+#include "/var/lib/gems/2.7.0/gems/ceedling-0.31.1/vendor/unity/src/unity.h"
+
+
+void test_leds_apagados_al_inicializar()
+
+{
+
+    UnityFail( (("empezamos")), (UNITY_UINT)(5));
+
+}
---
+++ leds_/test/test_leds_.c
@@ -0,0 +1,6 @@
+#include "unity.h"
+
+void test_leds_apagados_al_inicializar()
+{
+    TEST_FAIL_MESSAGE("empezamos");
+}
