31e873853343be04b17c1f4921661cf6ccc6e5f8
ldiazcharris <92344583+ldiazcharris@users.noreply.github.com>
2022-07-17 11:08:36 -0500

    Delete Build directory

--- leds_/build/test/dependencies/api_leds.d
+++
@@ -1,1 +0,0 @@
-build/test/out/c/api_leds.o: src/api_leds.c src/api_leds.h src/errores.h
--- leds_/build/test/dependencies/test_leds_.d
+++
@@ -1,4 +0,0 @@
-build/test/out/c/test_leds_.o: test/test_leds_.c \
- /var/lib/gems/2.7.0/gems/ceedling-0.31.1/vendor/unity/src/unity.h \
- /var/lib/gems/2.7.0/gems/ceedling-0.31.1/vendor/unity/src/unity_internals.h \
- src/api_leds.h build/test/mocks/mock_errores.h src/errores.h
